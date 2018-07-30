3f1fd0c2155644c5aa167a66535e995f90ef427f
Defiancefew <Severancepwns@gmail.com>
2018-07-31 02:29:41 +0300

    add constants

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const userNames = {
+  0: 'Aracely Walter',
+  1: 'Kelly Mante',
+  2: 'Matt Ankunding',
+  3: 'Vyacheslav Minin',
+};
+
+export const rooms = {
+  0: 'dev',
+  1: 'random',
+};
