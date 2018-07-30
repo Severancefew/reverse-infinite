b141220180f5d0475a64388c287501811d73f8e0
Defiancefew <Severancepwns@gmail.com>
2018-07-31 02:37:59 +0300

    add mui theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ChatLayout from './chat/ChatLayout';
+import { BrowserRouter } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+
+// A theme with custom primary and secondary color.
+// It's optional.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: purple[300],
+      main: purple[500],
+      dark: purple[700],
+    },
+    secondary: {
+      light: green[300],
+      main: green[500],
+      dark: green[700],
+    },
+  },
+});
 
 class App extends Component {
   render() {
-    return <div className="App">1</div>;
+    return (
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <ChatLayout />
+        </BrowserRouter>
+      </MuiThemeProvider>
+    );
   }
 }
 
