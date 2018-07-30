e570d4b4254f2bccde9aab86f3372bcb07633fb5
Defiancefew <Severancepwns@gmail.com>
2018-07-31 02:34:53 +0300

    chat list

---
+++ src/chat/ChatList.js
@@ -0,0 +1,218 @@
+import React from 'react';
+import ChatMessage from './ChatMessage';
+import {
+  List,
+  AutoSizer,
+  CellMeasurer,
+  InfiniteLoader,
+} from 'react-virtualized';
+import { isNaN } from 'lodash-es';
+import { isRecentMessage, isRecentDay } from '../utils/isRecent';
+
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  header: {
+    margin: 0,
+  },
+};
+
+class ChatList extends React.Component {
+  static defaultProps = {
+    history: {
+      offset: 0,
+      messages: [],
+      rowCount: 0,
+    },
+  };
+
+  state = {
+    scrollToIdx: -1,
+    loadByLink: false,
+    initial: true,
+  };
+
+  componentDidMount() {
+    if (this.getMessageNumber()) {
+      return this.jumpToMessage();
+    }
+
+    return this.props.fetchMore();
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    if (
+      prevProps.history.messages.length === 0 &&
+      this.props.history.messages.length > 0
+    ) {
+      return true;
+    }
+
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.params.channel !== this.props.params.channel) {
+      this.props.fetchMore();
+    }
+
+    // on initial load scroll to bottom
+    if (snapshot !== null) {
+      return this.scrollToIndex({ idx: 49, initial: true });
+    }
+  }
+
+  getMessageNumber = () => {
+    const {
+      params: { message },
+    } = this.props;
+
+    return isNaN(parseInt(message, 10)) ? false : parseInt(message, 10);
+  };
+
+  isScrolledAlready = (scrollTo, currentScroll) => scrollTo === currentScroll;
+
+  jumpToMessage = () => {
+    this.setState({ loadByLink: true }, () => {
+      const { history, fetchMore } = this.props;
+      const { loadByLink, scrollToIdx } = this.state;
+
+      const minimalMessageOffset = 20;
+      const messageInt = this.getMessageNumber();
+      const amountToFetch =
+        history.messages.length < messageInt
+          ? messageInt + minimalMessageOffset
+          : 0;
+
+      if (
+        messageInt &&
+        !this.isScrolledAlready(messageInt, scrollToIdx) &&
+        loadByLink
+      ) {
+        if (amountToFetch) {
+          fetchMore(amountToFetch, () => {
+            this.scrollToIndex({ idx: messageInt });
+          });
+        } else {
+          this.scrollToIndex({ idx: messageInt });
+        }
+      }
+
+      this.setState({ loadByLink: false });
+    });
+  };
+
+  scrollToIndex = ({ idx, initial }) => {
+    const { changeRowCounter } = this.props;
+
+    if (this.isScrolledAlready(this.state.scrollToIdx, idx)) {
+      return;
+    }
+
+    // just to ensure we won't trigger infinite right after rows rendered
+    this.setState(
+      {
+        scrollToIdx: idx,
+      },
+      () => {
+        if (initial) {
+          this.setState(
+            {
+              initial: false,
+            },
+            () => changeRowCounter(idx),
+          );
+        }
+      },
+    );
+  };
+
+  isRowLoaded = ({ index }) => {
+    if (this.props.isLoading || this.state.initial) {
+      return true;
+    }
+
+    return index > 0;
+  };
+
+  rowRender = ({ index, key, parent, style }) => {
+    const {
+      history: { messages },
+      currentRoom,
+      classes,
+      cache,
+    } = this.props;
+    const message = messages[index];
+    // used to merge messages
+    const hasRecentBefore =
+      index !== 0 && isRecentMessage(messages[index - 1], message);
+    const hasRecentDay =
+      index !== 0 && isRecentDay(messages[index - 1], message);
+
+    const date = new Date(message.timestamp);
+
+    return (
+      <CellMeasurer
+        parent={parent}
+        cache={cache}
+        columnIndex={0}
+        key={key}
+        rowIndex={index}
+      >
+        <div key={key} style={style} className={classes.row}>
+          {hasRecentDay && (
+            <h3 className={classes.header}>
+              {`${date.getFullYear()}/${date.getMonth()}/${date.getDate()}`}
+            </h3>
+          )}
+          <ChatMessage
+            date={date}
+            hasRecentBefore={hasRecentBefore}
+            {...message}
+            currentRoom={currentRoom}
+          />
+        </div>
+      </CellMeasurer>
+    );
+  };
+
+  render() {
+    const { history, fetchMore, classes, cache } = this.props;
+
+    return (
+      <InfiniteLoader
+        minimumBatchSize={1}
+        isRowLoaded={this.isRowLoaded}
+        threshold={0}
+        rowCount={history.rowCount}
+        loadMoreRows={() => fetchMore()}
+      >
+        {({ onRowsRendered, registerChild }) => (
+          <AutoSizer onResize={this.onResize}>
+            {({ width, height }) => (
+              <List
+                ref={ref => {
+                  this.props.setRef(ref);
+
+                  registerChild(ref);
+                }}
+                className={classes.list}
+                onRowsRendered={onRowsRendered}
+                deferredMeasurementCache={cache}
+                rowHeight={cache.rowHeight}
+                rowCount={history.messages.length}
+                rowRenderer={this.rowRender}
+                width={width}
+                height={height}
+                scrollToIndex={this.state.scrollToIdx}
+                scrollToAlignment={'center'}
+              />
+            )}
+          </AutoSizer>
+        )}
+      </InfiniteLoader>
+    );
+  }
+}
+
+export default withStyles(styles)(ChatList);
